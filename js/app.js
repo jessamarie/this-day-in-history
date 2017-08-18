47bb24b95d538509021b4cb04fdabd243a6e0f24
Jessa Marie Barre <jmlbarre@gmail.com>
2017-08-18 14:14:58 -0400

    worked on api service

--- js/app.js
+++ js/app.js
@@ -26,7 +26,7 @@ angular
   ])
 
 function factService ($http, $sce) {
-  var url = 'http://history.muffinlabs.com/date/8/12'
+  var url = 'http://history.muffinlabs.com/date'
   var trustedUrl = $sce.trustAsResourceUrl(url)
 
   return {
@@ -34,23 +34,32 @@ function factService ($http, $sce) {
   }
 
   function get () {
-    $http({
-      url: $sce.trustAsResourceUrl(url),
-      method: 'JSONP'
-    }).then(
-   function (data) {
-     $scope.myData = data
-     console.log(data);
-   },
-   function (error) {
-      // manage error
-   }
-)
+    $http.jsonp(trustedUrl, {jsonpCallbackParam: 'callback'})
+      .success(function (data) {
+        console.log(data)
+        return data
+      })
+    .error(function (data) {
+      console.log('failure')
+    })
+  }
+  //   $http({
+  //     url: $sce.trustAsResourceUrl(url),
+  //     method: 'JSONP'
+  //   }).then(
+  //  function (data) {
+  //    $scope.myData = data
+  //    console.log(data);
+  //  },
+  //  function (error) {
+  //     // manage error
+  //  }
+// )
     // $http.jsonp(trustedUrl, {jsonpCallbackParam: 'callback'})
     //     .success(function (data) {
     //       console.log(data.found)
     //     })
-  }
+//  }
   // return $resource('http://history.muffinlabs.com/date', {}, {
   //   jsonp_query: {
   //     method: 'JSONP'
