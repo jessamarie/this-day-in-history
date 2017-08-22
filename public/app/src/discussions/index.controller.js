28bd809d23c4152c63f5c132748d51cd6749c2d7
Jessa Marie Barre <jmlbarre@gmail.com>
2017-08-22 18:20:35 -0400

    updated index controller

--- public/app/src/discussions/index.controller.js
+++ public/app/src/discussions/index.controller.js
@@ -12,20 +12,18 @@
 
   function DiscussionIndexController (Discussion) {
     this.discussions = Discussion.query()
-    console.log(this.discussions)
+    this.getDateString = getDateString
+  }
 
-    this.discussion = new Discussion()
+  /* getDateString creates a Date object out of the parameters
+    in discussion and returns a string formatted in a
+    readable way.
 
-    this.getDateString = function (discussion) {
-      var date = new Date(discussion.year, discussion.month, discussion.day)
-      var options = { year: 'numeric', month: 'long', day: 'numeric' }
-      return date.toLocaleDateString('en-US', options)
-    }
-
-    this.create = function () {
-      this.discussion.$save().then((res) => {
-      })
-    }
-    // this.message = "Hi I'm a message"
+    i.e. March 27, 2010
+  */
+  function getDateString (discussion) {
+    var date = new Date(discussion.year, discussion.month, discussion.day)
+    var options = { year: 'numeric', month: 'long', day: 'numeric' }
+    return date.toLocaleDateString('en-US', options)
   }
 })()
