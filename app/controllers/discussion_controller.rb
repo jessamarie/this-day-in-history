45e4f6afbc79f918f89c39140dee5ca5da13a55d
Admin <Tgafvert@gmail.com>
2017-08-22 14:52:55 -0400

    added json rendering to controllers

--- app/controllers/discussion_controller.rb
+++ app/controllers/discussion_controller.rb
@@ -2,45 +2,47 @@ class DiscussionsController < ApplicationController
   # index
   def index
     @discussions = Discussion.all
+    render json: @adiscussions, include: [:discussions, :comments]
   end
 
-  # new
-  def new
-    @discussion = Discussion.new
-  end
+  # # new
+  # def new
+  #   @discussion = Discussion.new
+  # end
 
   # create
   def create
     @discussion = Discussion.create!(discussion_params)
-    redirect_to discussion_path(@discussion), notice: "Be the first person to comment on this date!"
+    render json: @discussion, status: :created, location: @discussion
   end
 
   #show
   def show
     @discussion = Discussion.find(params[:id])
+    render json: @discussion, include: [:discussions, :comments]
   end
 
-  # edit
-  def edit
-    @discussion = Discussion.find(params[:id])
-  end
+  # # edit
+  # def edit
+  #   @discussion = Discussion.find(params[:id])
+  # end
 
 
-  # update
-  def update
-    @discussion = discussion.find(params[:id])
-    @discussion.update(discussion_params)
+  # # update
+  # def update
+  #   @discussion = discussion.find(params[:id])
+  #   @discussion.update(discussion_params)
+  #
+  #   redirect_to discussion_path(@discussion), notice: "Discussion was successfully updated."
+  # end
 
-    redirect_to discussion_path(@discussion), notice: "Discussion was successfully updated."
-  end
-
-  # destroy
-  def destroy
-    @discussion = discussion.find(params[:id])
-    @discussion.destroy
-
-    redirect_to discussions_path
-  end
+  # # destroy
+  # def destroy
+  #   @discussion = discussion.find(params[:id])
+  #   @discussion.destroy
+  #
+  #   redirect_to discussions_path
+  # end
 
   private
   def discussion_params
