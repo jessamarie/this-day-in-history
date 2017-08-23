6551ffb1385f8cc0596850e39837de09424a2b89
Jessa Marie Barre <jmlbarre@gmail.com>
2017-08-23 11:24:05 -0400

    added migration and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,13 @@
 
 Comment.destroy_all
 Discussion.destroy_all
-patricks_bday = Discussion.create!(year: "1994", month: "2", day: "20")
+patricks_bday = Discussion.create!(year: "1994", month: "2", day: "21")
 patricks_bday.comments.create!(text: 'No, this is Patrick')
+patricks_bday.comments.create!(text: 'No, this is Patrick also')
+patricks_bday.comments.create!(text: 'No, this is Patrick to')
+patricks_bday.comments.create!(text: 'My bday was actually yesterday guys')
+
+
 jessas_bday = Discussion.create!(year: "1994", month: "9", day: "12")
 jessas_bday.comments.create(text: 'This is the best day ever!!!')
+jessas_bday.comments.create(text: 'Jessa birthday.....')
