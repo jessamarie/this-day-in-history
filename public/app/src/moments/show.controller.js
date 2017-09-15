9b6f928452f9558ed88a26d390680284a49c96d4
Juan C Garcia <Juan.Garcia@generalassemb.ly>
2017-09-15 10:47:19 -0400

    feedback

--- public/app/src/moments/show.controller.js
+++ public/app/src/moments/show.controller.js
@@ -5,9 +5,10 @@
     .module('moments')
     .controller('MomentShowController', MomentShowController)
 
-  MomentShowController.$inject = ['Moment', '$stateParams']
+  MomentShowController.$inject = ['momentHandler', '$stateParams']
 
-  function MomentShowController (Moment, $stateParams) {
+  function MomentShowController (momentHandler, $stateParams) {
+    let self = this
     this.dateParams = $stateParams
     this.queryDateParams = {
       month: $stateParams.month,
@@ -16,53 +17,29 @@
 
     this.year = $stateParams.year
 
-    loadMoments(this)
-
-    this.filterByYear = function (object) {
-      return object.filter((item) => { return item.year === this.year.toString() })
+    let isEmpty = function (obj) {
+      return angular.equals(obj, [])
     }
 
-    /* load moments resolves the promise and
-      contains functions needed after a
-      promise is resolved
-     */
-    function loadMoments (self) {
-      // get all data for the specific date
-      return Moment.get(self.queryDateParams).$promise.then(
-          function (data) {
-            self.date = data.date
-            self.url = data.url
-            self.events = self.filterByYear(data.data.Events)
-            self.births = self.filterByYear(data.data.Births)
-            self.deaths = self.filterByYear(data.data.Deaths)
+    let getRandomEvent = function (moment) {
+      /* sets a random event that occured on this day */
+      self.randomEvent = checkForEasterEgg(moment.date, self.dateParams.month, self.dateParams.day, self.year)
+      if (self.randomEvent !== null) {
+        // do nothing, we have an easter egg
+      } else if (!isEmpty(moment.events)) {
+        self.randomEvent = moment.events[Math.floor(Math.random() * moment.events.length)].text
+      } else if (!isEmpty(moment.births)) {
+        self.randomEvent = 'Birth: ' + moment.births[Math.floor(Math.random() * moment.births.length)].text
+      } else if (!isEmpty(moment.deaths)) {
+        self.randomEvent = 'Death: ' + moment.deaths[Math.floor(Math.random() * moment.deaths.length)].text
+      } else {
+        // no easterEggs or events
+        self.randomEvent = `${self.date}, ${self.year} was a very boring day.`
+      }
+    }
 
-            self.isEmpty = function (obj) {
-              return angular.equals(obj, [])
-            }
-
-            /* sets a random event that occured on this day */
-            self.getRandomEvent = function () {
-              self.randomEvent = checkForEasterEgg(self.date, self.dateParams.month, self.dateParams.day, self.year)
-              if (self.randomEvent !== null) {
-                // do nothing, we have an easter egg
-              } else if (!self.isEmpty(self.events)) {
-                self.randomEvent = self.events[Math.floor(Math.random() * self.events.length)].text
-              } else if (!self.isEmpty(self.births)) {
-                self.randomEvent = "Birth: " + self.births[Math.floor(Math.random() * self.births.length)].text
-              } else if (!self.isEmpty(self.deaths)) {
-                self.randomEvent = "Death: " + self.deaths[Math.floor(Math.random() * self.deaths.length)].text
-              } else {
-                // no easterEggs or events
-                self.randomEvent = `${self.date}, ${self.year} was a very boring day.`
-              }
-            } // end get random event
-            self.getRandomEvent()
-          },
-          function (error) {
-            console.log(error)
-          }
-        )
-    } // end load moments
+    // loadMoments(this)
+    momentHandler.getMoments($stateParams).then(getRandomEvent)
   }
 
   function checkForEasterEgg (date, month, day, year) {
--- public/app/src/services/momentHandler.js
+++ public/app/src/services/momentHandler.js
@@ -14,29 +14,79 @@ angular
 momentHandlerService.$inject = ['Moment']
 
 function momentHandlerService (Moment) {
-  var self = this
-  this.moments = {}
+  var moments = {}
 
-  this.filterByYear = function (object, year) {
+  let storeMoment = (date, moment) => {
+    moments[date.month] = moments[date.month] || []
+    if (!moments[date.month][date.day]) {
+      moments[date.month][date.day] = moment
+    }
+    return moments[date.month][date.day]
+  }
+
+  let findMoment = (date) => {
+    if (moments[date.month] && moments[date.month][date.day]) {
+      return moments[date.month][date.day]
+    } else {
+      return null
+    }
+  }
+
+  let filterByYear = function (object, year) {
     return object.filter((item) => { return item.year === year.toString() })
   }
 
-  this.isEmpty = function (obj) {
+  let isEmpty = function (obj) {
     return angular.equals(obj, [])
   }
 
+  let cleanRawMomentData = (data) => {
+    let moment = {}
+    moment.date = data.date
+    moment.url = data.url
+    moment.events = data.data.Events
+    moment.births = data.data.Births
+    moment.deaths = data.data.Deaths
+
+    return moment
+  }
+
+  let filterMomentByYear = (moment, year) => {
+    let filteredMoment = {}
+
+    filteredMoment.date = moment.date
+    filteredMoment.url = moment.url
+    filteredMoment.events = filterByYear(moment.events, year)
+    filteredMoment.births = filterByYear(moment.births, year)
+    filteredMoment.deaths = filterByYear(moment.deaths, year)
+
+    return filteredMoment
+  }
+
+  let momentHasYear = (moment, year) => {
+    var filteredMoment = filterMomentByYear(moment, year)
+
+    return !isEmpty(filteredMoment.events) ||
+           !isEmpty(filteredMoment.births) ||
+           !isEmpty(filteredMoment.deaths)
+  }
+
   function check (date) {
+    // see if the date was previously loaded before making an API call
+    let moment = findMoment(date)
+    if (moment && momentHasYear(moment, date.year)) {
+      return new Promise((resolve, reject) => {
+        resolve(true)
+      })
+    }
+    // make a request if we don't have the moment cached
+    // get all data for the specific date
     return Moment.get({ month: date.month, day: date.day }).$promise.then(
         function (data) {
-          var events = self.filterByYear(data.data.Events, date.year)
-          var births = self.filterByYear(data.data.Births, date.year)
-          var deaths = self.filterByYear(data.data.Deaths, date.year)
-
-          var exists = !self.isEmpty(events) ||
-                 !self.isEmpty(births) ||
-                 !self.isEmpty(deaths)
-
-          return exists
+          // store whole moment without trimming years
+          let moment = storeMoment(date, cleanRawMomentData(data))
+          // return whether moment has the requested year
+          return momentHasYear(moment, date.year)
         },
         function (error) {
           console.log(error)
@@ -45,20 +95,24 @@ function momentHandlerService (Moment) {
   } // end check
 
   function getMoments (date) {
+    let moment = findMoment(date)
+    if (moment) {
+      return new Promise((resolve, reject) => {
+        resolve(filterMomentByYear(moment, date.year))
+      })
+    }
+    // make a request if we don't have the moment cached
     // get all data for the specific date
     return Moment.get({ month: date.month, day: date.day}).$promise.then(
-      function (data) {
-        self.moments.date = data.date
-        self.moments.url = data.url
-        self.moments.events = self.filterByYear(data.data.Events, date.year)
-        self.moments.births = self.filterByYear(data.data.Births, date.year)
-        self.moments.deaths = self.filterByYear(data.data.Deaths, date.year)
-
-        return self.moments
-      },
-      function (error) {
-        console.log(error)
-      }
+        function (data) {
+          // store whole moment without trimming years
+          let moment = storeMoment(date, cleanRawMomentData(data))
+          // return only for the requested year
+          return filterMomentByYear(moment, date.year)
+        },
+        function (error) {
+          console.log(error)
+        }
       )
   }
 
